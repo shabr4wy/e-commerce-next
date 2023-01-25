2d2e8c312c3ba5c7a34d4869ed898d6e4673ea1e
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-25 16:30:24 +0200

    feat: create header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+/** @format */
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div>
+        <h1 className="header__shop-name"> Fake Shop </h1>
+      </div>
+
+      <div>
+        <a>Cart</a>
+        <a>Sign In</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
