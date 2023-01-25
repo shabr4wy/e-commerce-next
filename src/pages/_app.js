e827ce20a5a55a72de8c79af978c531ea53729d0
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-25 16:23:28 +0200

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
+/** @format */
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
