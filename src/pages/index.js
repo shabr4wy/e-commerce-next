b426d60c9e17c3d7accd4ed98d97aaa72c862a1e
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-26 22:41:50 +0200

    feat: fetch products data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 /** @format */
 
+export const getProducts = async () => {
+  const res = await fetch("https://dummyjson.com/products?limit=10");
+  const data = await res.json();
+
+  return data.products;
+};
+
+export async function getStaticProps() {
+  const products = await getProducts();
+  return { props: { products } };
+}
+
 export default function Home() {
   return <h1>Hello World!</h1>;
 }
