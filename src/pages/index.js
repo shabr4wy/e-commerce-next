4b0e8eaed2302851985ba081b6ca0315af2a79b1
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-26 23:26:16 +0200

    feat: fetch categories names

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
 /** @format */
 
+import Nav from "@/components/Nav";
 import ProductItem from "@/components/Product-item";
+
+export const getCategoriesNames = async () => {
+  const res = await fetch("https://dummyjson.com/products/categories");
+  const categoriesNames = await res.json();
+
+  return categoriesNames;
+};
 
 export const getProducts = async () => {
   const res = await fetch("https://dummyjson.com/products?limit=10");
@@ -11,7 +19,9 @@ export const getProducts = async () => {
 
 export async function getStaticProps() {
   const products = await getProducts();
-  return { props: { products } };
+  const categoriesNames = await getCategoriesNames();
+
+  return { props: { products, categoriesNames } };
 }
 
 export default function Home({ products }) {
