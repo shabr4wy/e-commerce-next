ac3bf6a9555ce409d23d76058b18233fb5dae5c4
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-29 19:50:13 +0200

    feat: apply routing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
 /** @format */
 
+import Link from "next/link";
+
 const Nav = ({ categoriesNames }) => {
   return (
     <nav className="nav">
       <ul>
         {categoriesNames?.map((categoryName) => (
-          <li key={categoryName}>{categoryName}</li>
+          <li key={categoryName}>
+            <Link href={`/categories/${categoryName}`}>{categoryName}</Link>
+          </li>
         ))}
       </ul>
     </nav>
