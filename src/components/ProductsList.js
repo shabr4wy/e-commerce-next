5ab86715ca14235102525c70f106b28b17889f19
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-29 23:07:50 +0200

    feat: apply routing for single products

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,11 +1,15 @@
 /** @format */
 
+import Link from "next/link";
+
 const ProductsList = ({ productsData }) => {
   return (
     <main>
       <ul>
         {productsData?.map((product) => (
-          <li key={product.title}>{product.title}</li>
+          <li key={product.title}>
+            <Link href={`/products/${product.id}`}>{product.title}</Link>
+          </li>
         ))}
       </ul>
     </main>
