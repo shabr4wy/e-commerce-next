f359e6ce6c72cab9ceedada2c73195586def0c4d
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-29 22:13:33 +0200

    feat: fetch all product data

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,5 +1,14 @@
 /** @format */
 
+export const getAllProducts = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+
+  const allProducts = data.products;
+
+  return allProducts;
+};
+
 const Product = ({ productData }) => {
   return <h1>{productData.title}</h1>;
 };
