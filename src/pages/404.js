8e6d6b091660c299f9b4b7bc2aba620b48061bd6
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-29 19:51:46 +0200

    feat: create 404 component to handle error

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+/** @format */
+
+export default function ErrorPage() {
+  return (
+    <div className="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+    </div>
+  );
+}
