c4076b2bfb0f759519d11f1d8c47dbb812c01c9b
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-01-29 19:35:58 +0200

    feat: get all categories paths

--- src/pages/categories/[category-products].js
+++ src/pages/categories/[category-products].js
@@ -1,5 +1,36 @@
 /** @format */
 
+export const getCategoriesNames = async () => {
+  const res = await fetch("https://dummyjson.com/products/categories");
+  const categoriesNames = await res.json();
+
+  return categoriesNames;
+};
+
+export const getCategoriesPaths = (categoriesNames) => {
+  const paths = categoriesNames.map((categoryName) => {
+    return { params: { "category-products": categoryName } };
+  });
+
+  return paths;
+};
+
+export async function getStaticPaths() {
+  const categoriesNames = await getCategoriesNames();
+  const paths = await getCategoriesPaths(categoriesNames);
+
+  return { paths, fallback: false };
+}
+
+const getCategoriesProductData = async (context) => {
+  const res = await fetch(
+    `https://dummyjson.com/products/category/${context.params.category}`
+  );
+  const data = await res.json();
+
+  return data;
+};
+
 const CategoryProducts = () => {
   return <h1>CategoryProducts</h1>;
 };
