1c34392b0e94da543529ca39b043a6c98cf8afb7
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-03-15 19:37:59 +0200

    feat: create product page component

---
+++ src/components/Product-page.js
@@ -0,0 +1,55 @@
+/** @format */
+
+const ProductPage = ({ productData }) => {
+  return (
+    <main className="product">
+      <section className="product__images">
+        {productData?.images?.map((image) => (
+          <img src={image} alt={productData?.description} key={image} />
+        ))}
+      </section>
+
+      <section className="product__title">
+        <h3>{productData?.title}</h3>
+      </section>
+
+      <section className="product__description">
+        <p>{productData?.description}</p>
+      </section>
+
+      <section className="product__price">
+        <span>{"$" + productData?.price}</span>
+      </section>
+
+      <section className="product__quantity">
+        <label htmlFor="productQuantity">quantity</label>
+
+        <button
+          className="product__decreseItems"
+          aria-labelledby="decrease items quantity by 1"
+        >
+          -
+        </button>
+
+        <input
+          type="number"
+          id="productQuantity"
+          name="productQuantity"
+        ></input>
+
+        <button
+          className="product__increseItems"
+          aria-labelledby="increse items quantity by 1"
+        >
+          +
+        </button>
+      </section>
+
+      <section>
+        <button className="product__addToCartBtn">Add to cart</button>
+      </section>
+    </main>
+  );
+};
+
+export default ProductPage;
