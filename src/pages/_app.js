c1d603f33e51b8780a2499e7493574ccbf40b44d
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-03-23 16:19:09 +0200

    feat: use context to share cart between pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 /** @format */
 
+import { createContext, useCallback, useState } from "react";
+
+export const CartContext = createContext([]);
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [cart, setCart] = useState([]);
+
+  const updateCart = useCallback(
+    (newProduct) => {
+      setCart([...cart, newProduct]);
+    },
+    [cart]
+  );
+
+  return (
+    <CartContext.Provider value={{ cart, updateCart }}>
+      <Component {...pageProps} />
+    </CartContext.Provider>
+  );
 }
