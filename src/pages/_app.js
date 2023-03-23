bacaa2a0687089d2ed8bb0fedc0ac79999bd91d3
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-03-23 20:52:23 +0200

    feat: creat items number  context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,11 @@
 import { createContext, useCallback, useState } from "react";
 
 export const CartContext = createContext([]);
+export const ItemsNumberContext = createContext(1);
 
 export default function App({ Component, pageProps }) {
   const [cart, setCart] = useState([]);
-  console.log(cart);
+  const [itemsNumber, setItemsNumber] = useState(1);
 
   const updateCart = useCallback(
     (newProduct) => {
@@ -17,7 +18,9 @@ export default function App({ Component, pageProps }) {
 
   return (
     <CartContext.Provider value={{ cart, updateCart }}>
-      <Component {...pageProps} />
+      <ItemsNumberContext.Provider value={{ itemsNumber, setItemsNumber }}>
+        <Component {...pageProps} />
+      </ItemsNumberContext.Provider>
     </CartContext.Provider>
   );
 }
