3c0576a95c12fbf7eea401114d0a8f05151a5773
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-03-25 23:32:01 +0200

    feat: use local storage functions in App component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,24 @@
 /** @format */
-
 import { createContext, useCallback, useState } from "react";
+
+import {
+  cart_From_Local_Sotrage,
+  get_Cart_Form_Local_storage,
+  add_Item_to_Cart_In_Local_Storage,
+} from "@/modules/localStorage";
 
 export const CartContext = createContext([]);
 export const ItemsNumberContext = createContext(1);
 
 export default function App({ Component, pageProps }) {
-  const [cart, setCart] = useState([]);
   const [itemsNumber, setItemsNumber] = useState(1);
+  const [cart, setCart] = useState(
+    cart_From_Local_Sotrage ? cart_From_Local_Sotrage : []
+  );
+
+  get_Cart_Form_Local_storage();
+
+  add_Item_to_Cart_In_Local_Storage(cart);
 
   const updateCart = useCallback(
     (newProduct) => {
