a0435e92e8952999417a8309d00c531e25e22761
Abdullah Elshabrawy <abdullahyasinforwork@gmail.com>
2023-03-30 14:09:47 +0200

    feat: handle empty cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -15,32 +15,44 @@ const CartPage = () => {
       <Header />
       <Nav />
       <main className="cart">
-        <>
-          <section className="cart__Products">
-            {cart.map((product) => (
-              <>
-                <h3>{product.title}</h3>
-                <div>{product.itemsBeingPurshased}</div>
-                <div>{product.price}</div>
-              </>
-            ))}
-          </section>
+        {cart.length > 0 ? (
+          <>
+            <section className="cart__Products">
+              {cart.map((product) => (
+                <>
+                  <h3>{product.title}</h3>
+                  <div>{product.itemsBeingPurshased}</div>
+                  <div>{product.price}</div>
+                </>
+              ))}
+            </section>
 
-          <section className="cart__total price">
-            Total price: $
-            {cart.reduce(
-              (prevValue, currValue) =>
-                prevValue +
-                Number(currValue.price) * Number(currValue.itemsBeingPurshased),
-              0
-            )}
-          </section>
+            <section className="cart__total price">
+              Total price: $
+              {cart.reduce(
+                (prevValue, currValue) =>
+                  prevValue +
+                  Number(currValue.price) *
+                    Number(currValue.itemsBeingPurshased),
+                0
+              )}
+            </section>
 
-          <section className="cart__proceedToBuy">
-            {" "}
-            <a href="">Proceed to Buy</a>
-          </section>
-        </>
+            <section className="cart__proceedToBuy">
+              {" "}
+              <a href="">Proceed to Buy</a>
+            </section>
+          </>
+        ) : (
+          <>
+            <div>
+              <p> Your cart is empty</p>
+            </div>
+            <div>
+              <Link href={"/pages/"}>Back to products</Link>
+            </div>
+          </>
+        )}
       </main>
     </>
   );
